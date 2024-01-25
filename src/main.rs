a0d6eb262450aef782cec060ac2f67e70f4284ae
ManiGhazaee <hosseinghazaee1@gmail.com>
2024-01-26 01:29:25 +0330

    refactor: perf improvement

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,28 @@
 #![allow(dead_code)]
 use std::{
     cmp::min,
-    fs::{self, read_dir, File, OpenOptions},
-    io::{stdout, Read, Seek, SeekFrom, Write},
+    fmt::Error,
+    fs::{read_dir, DirEntry, File, OpenOptions},
+    io::{Read, Seek, SeekFrom, Write},
     path::{Path, PathBuf},
     sync::{Arc, Mutex},
     time::Instant,
 };
 
-use rayon::iter::{
-    IntoParallelIterator, IntoParallelRefIterator, IntoParallelRefMutIterator, ParallelIterator,
+use rayon::{
+    iter::{
+        IndexedParallelIterator, IntoParallelIterator, IntoParallelRefIterator, ParallelBridge,
+        ParallelIterator,
+    },
+    str::ParallelString,
 };
-use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, StandardStreamLock, WriteColor};
+use regex::Regex;
+use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 
 #[derive(Debug)]
 struct Match {
     ln: usize,
     col: Vec<usize>,
-    buf_s: Vec<usize>,
-    buf_e: Vec<usize>,
     ln_str: String,
 }
 
@@ -34,34 +38,45 @@ fn main() {
     // let mut file_string = String::new();
     // let path = "../typescript-typechecker/";
     let path = "../typescript-typechecker/";
-    let pat = "Vec";
+    let pat = "fn";
     let pat_len = pat.len();
 
     // let mut paths: Vec<PathBuf> = Vec::new();
     // get_paths(&mut paths, Path::new(path).into());
 
-    // paths.par_iter_mut().for_each(|path| {
+    // for path in paths {
+    //     if path.file_name().unwrap() == ".gitignore" {
+    //         println!("{:?}", path);
+    //     }
+    // }
+
+    // let matches_res: Arc<Mutex<Vec<Matches>>> = Arc::new(Mutex::new(Vec::new()));
+    // paths.par_iter().for_each(|path| {
     //     let mut file_string = String::new();
     //     let mut file = match OpenOptions::new().read(true).open(&path) {
     //         Ok(v) => v,
     //         Err(_) => return,
     //     };
+    //     if is_binary(&mut file) {
+    //         return;
+    //     }
     //     if let Err(_) = file.read_to_string(&mut file_string) {
     //         return;
     //     }
-    //     if file_string[0..min(file_string.len(), 1024)].contains('\x00') {
-    //         return;
-    //     }
-    //     let matches = find_matches(&mut file_string, &pat, pat_len);
-    //     print_matches(&matches, &path);
+    //     let matches = find_matches_par(&mut file_string, &pat, pat_len);
+    //     matches_res.lock().unwrap().push(Matches {
+    //         path: path.to_owned(),
+    //         m: matches,
+    //     });
     // });
 
     // match_recursively(&Path::new(path), &mut file_string, pat, pat_len);
-    let matches_res: Arc<Mutex<Vec<Matches>>> = Arc::new(Mutex::new(Vec::new()));
-    match_par_2(&Path::new(path), pat, pat_len, &matches_res);
-    matches_res.lock().unwrap().par_iter().for_each(|matches| {
-        print_matches(&matches.m, &matches.path, pat_len);
-    });
+    match_par(Path::new(path), pat, pat_len);
+    // let matches_res: Arc<Mutex<Vec<Matches>>> = Arc::new(Mutex::new(Vec::new()));
+    // match_par_2(&Path::new(path), pat, pat_len, &matches_res);
+    // matches_res.lock().unwrap().par_iter().for_each(|matches| {
+    //     print_matches(&matches.m, &matches.path, pat_len);
+    // });
     println!("{}ms", inst.elapsed().as_millis())
 }
 
@@ -70,62 +85,13 @@ fn get_paths(result: &mut Vec<PathBuf>, path: PathBuf) {
         result.push(path);
     } else if path.is_dir() {
         for entry in read_dir(path).unwrap() {
-            let entry = entry.unwrap();
-            let path = entry.path();
-            if path.is_dir() {
-                get_paths(result, path);
-            } else if path.is_file() {
-                result.push(path);
-            }
+            let path = entry.unwrap().path();
+            get_paths(result, path);
         }
     }
 }
 
-fn match_recursively(path: &Path, file_string: &mut String, pat: &str, pat_len: usize) {
-    file_string.clear();
-    if path.is_file() {
-        let mut file = match OpenOptions::new().read(true).open(&path) {
-            Ok(v) => v,
-            Err(_) => return,
-        };
-        if let Err(_) = file.read_to_string(file_string) {
-            return;
-        }
-        if file_string[0..min(file_string.len(), 1024)].contains('\x00') {
-            return;
-        }
-        let matches = find_matches(file_string, &pat, pat_len);
-        print_matches(&matches, &path, pat_len);
-        file_string.clear();
-        return;
-    } else if path.is_dir() {
-        for entry in read_dir(path).unwrap() {
-            let entry = entry.unwrap();
-            let path = entry.path();
-
-            if path.is_dir() {
-                match_recursively(&path, file_string, pat, pat_len);
-            } else if path.is_file() {
-                let mut file = match OpenOptions::new().read(true).open(&path) {
-                    Ok(v) => v,
-                    Err(_) => continue,
-                };
-                if let Err(_) = file.read_to_string(file_string) {
-                    continue;
-                }
-                if file_string[0..min(file_string.len(), 1024)].contains('\x00') {
-                    return;
-                }
-
-                let matches = find_matches(file_string, &pat, pat_len);
-                print_matches(&matches, &path, pat_len);
-                file_string.clear()
-            }
-        }
-    }
-}
-
-fn match_recursively_par(path: &Path, pat: &str, pat_len: usize) {
+fn match_par(path: &Path, pat: &str, pat_len: usize) {
     if path.is_file() {
         let mut file_string = String::new();
         let mut file = match OpenOptions::new().read(true).open(&path) {
@@ -138,32 +104,23 @@ fn match_recursively_par(path: &Path, pat: &str, pat_len: usize) {
         if let Err(_) = file.read_to_string(&mut file_string) {
             return;
         }
-
-        let matches = find_matches(&file_string, &pat, pat_len);
+        let matches = find_matches_par(&file_string, &pat, pat_len);
         print_matches(&matches, &path, pat_len);
         return;
     } else if path.is_dir() {
-        let entires: Vec<_> = read_dir(path).unwrap().filter_map(|e| e.ok()).collect();
-        entires.par_iter().for_each(|entry| {
-            let path = entry.path();
-
-            // if path.is_dir() {
-            match_recursively_par(&path, pat, pat_len);
-            // }
-            // let mut file = match OpenOptions::new().read(true).open(&path) {
-            //     Ok(v) => v,
-            //     Err(_) => return,
-            // };
-            // if is_binary(&mut file) {
-            //     return;
-            // }
-            // if let Err(_) = file.read_to_string(&mut file_string) {
-            //     return;
-            // }
-
-            // let matches = find_matches(&file_string, &pat, pat_len);
-            // print_matches(&matches, &path);
-        })
+        read_dir(path)
+            .unwrap()
+            .into_iter()
+            .par_bridge()
+            .for_each(|entry| {
+                let path = entry.unwrap().path();
+                match_par(&path, pat, pat_len);
+            });
+        // let entires: Vec<_> = read_dir(path).unwrap().filter_map(|e| e.ok()).collect();
+        // entires.par_iter().for_each(|entry| {
+        //     let path = entry.path();
+        //     match_par(&path, pat, pat_len);
+        // })
     }
 }
 
@@ -180,26 +137,30 @@ fn match_par_2(path: &Path, pat: &str, pat_len: usize, matches_res: &Arc<Mutex<V
         if let Err(_) = file.read_to_string(&mut file_string) {
             return;
         }
-
-        let matches = find_matches(&file_string, &pat, pat_len);
-        let mut lock = matches_res.lock().unwrap();
-        lock.push(Matches {
+        let matches = find_matches_par(&file_string, &pat, pat_len);
+        matches_res.lock().unwrap().push(Matches {
             m: matches,
             path: path.into(),
         });
-        return;
     } else if path.is_dir() {
-        let entires: Vec<_> = read_dir(path).unwrap().filter_map(|e| e.ok()).collect();
-        entires.par_iter().for_each(|entry| {
-            let path = entry.path();
-            match_par_2(&path, pat, pat_len, matches_res);
-        })
+        read_dir(path)
+            .unwrap()
+            .into_iter()
+            .par_bridge()
+            .for_each(|entry| {
+                let path = entry.unwrap().path();
+                match_par_2(&path, pat, pat_len, matches_res);
+            });
+        // let entires: Vec<_> = read_dir(path).unwrap().filter_map(|e| e.ok()).collect();
+        // entires.par_iter().for_each(|entry| {
+        //     let path = entry.path();
+        //     match_par_2(&path, pat, pat_len, matches_res);
+        // })
     }
 }
 
 fn find_matches(file_string: &String, pat: &str, pat_len: usize) -> Vec<Match> {
     let mut matches: Vec<Match> = Vec::new();
-    let mut buf_idx = 0;
     for (line_idx, line) in file_string.lines().enumerate() {
         if pat_len > line.len() {
             continue;
@@ -210,24 +171,48 @@ fn find_matches(file_string: &String, pat: &str, pat_len: usize) -> Vec<Match> {
                     if last.ln == line_idx + 1 {
                         let last_idx = matches.len() - 1;
                         matches[last_idx].col.push(i + 1);
-                        matches[last_idx].buf_s.push(buf_idx);
-                        matches[last_idx].buf_e.push(buf_idx + pat_len);
-                        buf_idx += 1;
                         continue;
                     }
                 }
                 matches.push(Match {
                     ln: line_idx + 1,
                     col: Vec::from([i + 1]),
-                    buf_s: Vec::from([buf_idx]),
-                    buf_e: Vec::from([buf_idx + pat_len]),
                     ln_str: line.to_string(),
                 });
             }
-            buf_idx += 1;
         }
     }
     matches
+}
+
+fn find_matches_par(file_string: &str, pat: &str, pat_len: usize) -> Vec<Match> {
+    let matches: Arc<Mutex<Vec<Match>>> = Arc::new(Mutex::new(Vec::new()));
+    file_string
+        .lines()
+        .enumerate()
+        .par_bridge()
+        .for_each(|(line_idx, line)| {
+            if pat_len > line.len() {
+                return;
+            }
+            let mut col: Vec<usize> = Vec::new();
+            let mut i = 0;
+            while i < line.len() - pat_len {
+                if &line[i..i + pat_len] == pat {
+                    col.push(i + 1);
+                }
+                i += 1;
+            }
+            if col.is_empty() {
+                return;
+            }
+            matches.lock().unwrap().push(Match {
+                ln: line_idx + 1,
+                col,
+                ln_str: line.to_string(),
+            })
+        });
+    Arc::try_unwrap(matches).unwrap().into_inner().unwrap()
 }
 
 fn print_matches(matches: &Vec<Match>, path: &Path, pat_len: usize) {
